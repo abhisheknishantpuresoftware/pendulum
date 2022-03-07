5f7b4dbf06b9578a4322870da57a38a7547ecc27
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2022-03-07 13:30:11 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,12 +5,12 @@ cd $(dirname $0)
 
 export PATH=/opt/python/cp38-cp38/bin/:$PATH
 
-curl -fsS -o get-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py
-/opt/python/cp38-cp38/bin/python get-poetry.py --preview -y --version 1.1.13
+#curl -fsS -o get-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py
+#/opt/python/cp38-cp38/bin/python get-poetry.py --preview -y --version 1.1.13
 rm get-poetry.py
-#curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/install-poetry.py
-#/opt/python/cp310-cp310/bin/python install-poetry.py --preview -y
-#export PATH="$HOME/.poetry/bin:$PATH"
+curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/install-poetry.py
+/opt/python/cp38-cp38/bin/python install-poetry.py --preview -y
+export PATH="$HOME/.poetry/bin:$PATH"
 
 for PYBIN in /opt/python/cp3*/bin; do
   if [ "$PYBIN" == "/opt/python/cp34-cp34m/bin" ]; then
@@ -20,8 +20,8 @@ for PYBIN in /opt/python/cp3*/bin; do
     continue
   fi
   rm -rf build
-  #"${PYBIN}/python" poetry build -vvv
-  "${PYBIN}/python" $HOME/.poetry/bin/poetry build -vvv
+  "${PYBIN}/python" poetry build -vvv
+  #"${PYBIN}/python" $HOME/.poetry/bin/poetry build -vvv
 done
 
 cd dist
