a06af8af54f1fcc1343d88002da377963f52b510
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2022-03-07 14:34:15 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -23,6 +23,7 @@ for PYBIN in /opt/python/cp3*/bin; do
   rm -rf build
   #poetry env use ${PYBIN}/python
   poetry build -vvv
+  poetry env remove ${PYBIN}/python
   #"${PYBIN}/python" $HOME/.poetry/bin/poetry build -vvv
 done
 
