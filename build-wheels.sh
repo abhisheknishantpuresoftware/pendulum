beaba847ac7cb6c457b493e04f8598d6a217af9f
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2022-03-07 11:40:24 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -12,7 +12,7 @@ curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetr
 /opt/python/cp310-cp310/bin/python install-poetry.py --preview -y
 export PATH="/root/.local/bin:$PATH"
 
-$HOME/.poetry/bin/poetry --version
+#$HOME/.poetry/bin/poetry --version
 
 poetry --version
 
