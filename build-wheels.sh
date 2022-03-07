0d62316e2a79f04c26936c6924228e9e8021236b
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2022-03-07 13:23:32 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -3,14 +3,14 @@ set -e -x
 
 cd $(dirname $0)
 
-export PATH=/opt/python/cp310-cp310/bin/:$PATH
+export PATH=/opt/python/cp38-cp38/bin/:$PATH
 
 curl -fsS -o get-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py
 /opt/python/cp38-cp38/bin/python get-poetry.py --preview -y --version 1.1.13
 rm get-poetry.py
 #curl -fsS -o install-poetry.py https://raw.githubusercontent.com/sdispater/poetry/master/install-poetry.py
 #/opt/python/cp310-cp310/bin/python install-poetry.py --preview -y
-export PATH="$HOME/.poetry/bin:$PATH"
+#export PATH="$HOME/.poetry/bin:$PATH"
 
 for PYBIN in /opt/python/cp3*/bin; do
   if [ "$PYBIN" == "/opt/python/cp34-cp34m/bin" ]; then
