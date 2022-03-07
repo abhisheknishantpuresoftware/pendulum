c65dc714ec47a6004ca05a4fb2a553384eeb9065
abhisheknishantpuresoftware <66300308+abhisheknishantpuresoftware@users.noreply.github.com>
2022-03-07 11:38:58 +0530

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -14,6 +14,8 @@ export PATH="/root/.local/bin:$PATH"
 
 $HOME/.poetry/bin/poetry --version
 
+poetry --version
+
 for PYBIN in /opt/python/cp3*/bin; do
   if [ "$PYBIN" == "/opt/python/cp34-cp34m/bin" ]; then
     continue
